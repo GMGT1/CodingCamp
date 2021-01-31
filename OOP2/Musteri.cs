08fd43007f402fd6d089a7e13dad0c67f0322678
Talha Gamga <HaxTalha@hotmail.com>
2021-01-31 22:42:44 +0300

    31 Ocak Ders

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Classlar özellik ve operasyon tutar.
+    //Base class
+    class Musteri       
+    {
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+    }
+}
---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    /*
+     * İnterfaceleri birbirinin alternatifi olan ancak kod içerikleri farklı olan durumlar için kullanırız.
+     * İnterface içinde sadece imza metotları olur(kalıp-şablon) içi dolu olamaz çünkü onları biz özelleştiririz.
+    */
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+
+    }
+}
